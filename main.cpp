ea9f413dee5d385b2e6716465569872e03eeed4b
Rachel Ombok <rbo224@nyu.edu>
2020-03-09 11:33:21 -0400

    Second draft of SulSulSimSim C++ file

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-// First draft commit of sim sim
+// Second draft commit of sim sim
 //
 
 #include <iostream>
@@ -16,159 +16,75 @@ class Object {
     };
     
 public:
-    Object(const string& name) : name(name) {}
-    
-    void moodEffect(int value){
-        happiness += value;
-    }
+    Object(const string& name, vector<int>& effect) : name(name), effect(effect) {}
     
     const string& getName() const {return name;}
     void changeColor(const string& newcolor) {color = newcolor;}
     void changeCost(const double& newcost) {cost = newcost;}
-    virtual void characteristics(const string& itemcolor, const double& itemcost) = 0;
-    virtual int interact(Sim& sim) = 0;
+    const int getNeedValue(int i) {return effect[i];}
     
 private:
     string name;
     double cost;
     string color;
-    int happiness;
-    
-    //bool effect;
-    //positive = true
-    //negative = false
+    vector<int> effect;
     
 };
-
-
-
-class HungerObject : public Object{
-public:
-    HungerObject(const string& name, int interactval) : Object(name), val(interactval) {}
-        
-    void characteristics(const string& itemcolor, const double& itemcost) {
-        this->changeColor(itemcolor);
-        this->changeCost(itemcost);
-            //environment = environmentval;
-    }
-        
-    int interact(Sim& sim){
-        return val;
-    }
-        
-private:
-    int val;
-
-};
-
-class BladderObject : public Object{
-public:
-    BladderObject(const string& name, int interactval) : Object(name), val(interactval) {}
-        
-    void characteristics(const string& itemcolor, const double& itemcost) {
-        this->changeColor(itemcolor);
-        this->changeCost(itemcost);
-            //environment = environmentval;
-    }
-        
-    int interact(Sim& sim){
-        return val;
-    }
-        
-private:
-    int val;
-
-};
-
-class EnergyObject : public Object{
-public:
-    EnergyObject(const string& name, int interactval) : Object(name), val(interactval) {}
-        
-    void characteristics(const string& itemcolor, const double& itemcost) {
-        this->changeColor(itemcolor);
-        this->changeCost(itemcost);
-            //environment = environmentval;
-    }
-        
-    int interact(Sim& sim){
-        return val;
-    }
-        
-private:
-    int val;
-
-};
-
 
 class Sim{
     friend ostream& operator<<(ostream& os, const Sim& theSim){
         os << theSim.name << " has the following needs: " << endl;
-        os << "Hunger: " << theSim.hunger << "\n" << "Hygeine: " << theSim.hygeine << "\n" << "Bladder: " << theSim.bladder << "\n" << "Energy: " << theSim.energy << "\n" << "Social: " << theSim.social << "\n" << "Fun: " << theSim.fun << endl;
+        for(size_t i = 0; i < theSim.needs.size(); i++){
+            os << theSim.needs[i] << endl;
+        }
         return os;
     };
-public:
     
-    // void interact
-    // if a sim interacts w an object, it affects their needs
+public:
+
+    
     Sim(const string& name) : name(name) {}
     
-    void interactWith(HungerObject& object){
+    const string& getName() {return name;}
+    
+    void interactWith(Object& object){
         cout << name << " interacts with " << object.getName() << endl;
-        int valeffect = object.interact(*this) + hunger;
-        
-        if (valeffect > 10){
-            hunger = 10;
-        }
-        
-        else{
-            hunger = valeffect;
+        for (size_t i = 0; i < needs.size(); i++){
+            needs[i] += object.getNeedValue(i);
         }
         
     }
     
-    void interactWith(BladderObject& object){
-        cout << name << " interacts with " << object.getName() << endl;
-        int valeffect = object.interact(*this) + hunger;
-        if (valeffect > 10){
-            bladder = 10;
-        }
-        
-        else{
-            bladder = valeffect;
-        }
-    }
-    
-    void interactWith(EnergyObject& object){
-        cout << name << " interacts with " << object.getName() << endl;
-        int valeffect = object.interact(*this) + hunger;
-        if (valeffect > 10){
-            energy = 10;
-        }
-        
-        else{
-            energy = valeffect;
-        }
-    }
+
     
 private:
     //needs: hunger, hygeine, bladder, energy, social, fun
     string name;
-    int hunger = 5;
-    int hygeine = 5;
-    int bladder = 5;
-    int energy = 5;
-    int social = 5;
-    int fun = 5;
+    vector<int> needs{5,5,5,5,5,5};
     
 };
 
 class Room{
-    
+    friend ostream& operator<<(ostream& os, const Room& theRoom){
+        os << "The " << theRoom.name << " has the following objects inside: " << endl;
+        for(size_t i = 0; i < theRoom.room.size(); i++){
+            os << theRoom.room[i]->getName() << endl;
+        }
+        return os;
+    };
 public:
     Room(const string& name) : name(name) {}
     
     void add_object(Object& object){
         room.push_back(&object);
+    }
+    
+    void placeSim(Sim& sim){
+        //places sim in room, makes them interact w their environment
+        cout << sim.getName() << "has been placed in the " << name << endl;
+        for (size_t i = 0; i < room.size(); i++){
+            sim.interactWith(*room[i]);
+        }
     }
     
 private:
@@ -177,49 +93,64 @@ private:
     
 };
 
+// House Class to keep rooms
+
+class House{
+    
+public:
+    
+    void add_room(Room& room){
+        house.push_back(&room);
+    }
+    
+    void placeSim(Sim& sim){
+        cout << sim.getName() << " has been placed in the house" << endl;
+        for (size_t i = 0; i < house.size(); i++){
+            house[i]->placeSim(sim);
+        }
+    }
+    
+private:
+    vector<Room*> house;
+};
 
 int main() {
-    HungerObject fridge("fridge",3);
+    // Sim object, Rachel
     Sim rachel("Rachel");
     cout << rachel << endl;
+    
+    //needs order: hunger, hygeine, bladder, energy, social, fun
+    vector<int> fridge_fx{3,-1,-2,1,0,0};
+    vector<int> toilet_fx{0,-1,3,0,0,0};
+    vector<int> bed_fx{-1,-1,-1,3,-1,-1};
+    
+    // objects the sim can interact with and affect their needs
+    Object fridge("fridge", fridge_fx);
+    Object toilet("toilet", toilet_fx);
+    Object bed("bed", bed_fx);
+    
+    // sims can interact with individual objects, which will affect their needs
     rachel.interactWith(fridge);
     cout << rachel << endl;
     
-    Room r1("Living Room");
+    // Room object, which can hold different objects
+    Room livingroom("Living Room");
+    livingroom.add_object(fridge);
+    livingroom.add_object(toilet);
+    livingroom.add_object(bed);
+    cout << livingroom << endl;
+    
+    // Place a sim in a room, all the objects will affect the sims needs
+    livingroom.placeSim(rachel);
+    cout << rachel << endl;
+    
 
     return 0;
 }
 /*
- methods for happiness/positive item
- methods for negative bad item
- 
- method for appearance
- method for build, clothes, funiture etc
- method for expansion packs
- method for type ex: food, wallpaper, etc
- 
- think as a Sim, not a Player
- needs interact with objects
- make sim object
- needs with objects
+ Next steps:
+ > making cost a factor in need effect?
+ > House class? Interact with whole house? Or just container for rooms?
+ > If a room is in a house can we only interact with a certain room?
+ > Chamh
  */
-
-/*
-class Build : public Object {
-    
-public:
-    Build(const string& name) : Object(name) {}
-    void characteristics(const string& itemcolor, const double& itemcost) const{
-        
-    }
-private:
-    
-};
-*/
-
-//generic object that increases a specific need, only one object
-//positive and negative effect on objects
-//keep room vector option
-//vector of needs = [-1,2,3,4,-3,0]
-// negative 1 effect on hunger, plus 2 on energy, 3 on social, no effect on last one
-
